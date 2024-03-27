bba431b3e339761590ef324f8e62bd362b13cef9
Ibrahim Soltan <ibrahim.soltan918@gmail.com>
2024-03-27 06:24:10 +0200

    feat: previously generated quizzes stored

--- extension/js/index.js
+++ extension/js/index.js
@@ -1,7 +1,14 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const startButton=document.getElementById("start");
+document.addEventListener("DOMContentLoaded", function () {
+    const startButton = document.getElementById("start");
 
-    startButton.addEventListener("click", function(){
-        window.location.href="../html/text_input.html"
+    startButton.addEventListener("click", function () {
+        window.location.href = "../html/text_input.html"
     });
+
+    const pastQuizzesButton = document.getElementById("past_quizzes")
+
+
+    pastQuizzesButton.addEventListener("click", function () {
+        window.open("../html/past_quizzes.html", "_blank");
+    })
 });
