288fd8f13e7e3f3f48748576d9879e7222a6b29a
Krishna Madhwani <100672872+Roaster05@users.noreply.github.com>
2024-07-10 12:09:51 +0530

    added Wiki based content support and basic refactoring (#47)
    * added support for Wiki based content

--- backend/Generator/__init__.py
+++ backend/Generator/__init__.py
@@ -1,3 +1,3 @@
 # Constructor for questgen
 from __future__ import absolute_import
-from Generator.main import MCQGenerator, BoolQGenerator, ShortQGenerator, AnswerPredictor, GoogleDocsService, FileProcessor
+from Generator.main import MCQGenerator, BoolQGenerator, ShortQGenerator, AnswerPredictor, GoogleDocsService, FileProcessor, QuestionGenerator
