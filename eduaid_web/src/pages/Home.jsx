27a17bae3db13b55ca3f8952d32e0b1dc28b1533
IronJam11 <aaryanjain888@gmail.com>
2025-02-15 18:59:46 +0530

    fix: minor bug fixes

--- eduaid_web/src/pages/Home.jsx
+++ eduaid_web/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import "../index.css";
 import logo from "../assets/aossie_logo.png";
 import starsImg from "../assets/stars.png";
--- server/server/settings.py
+++ server/server/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zx6-it^h=%!eb+23+zjvpjfx3eged-ncx+kryx@3gs3sae81++'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
