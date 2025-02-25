d3ae507e5755e385191b843ec240f0b0dd3023d9
sakina <sakena1303@gmail.com>
2025-02-25 19:45:39 +0530

    Prettier formatting applied along with encompassment of .prettier.json & .prettierignore

--- eduaid_web/src/pages/Home.jsx
+++ eduaid_web/src/pages/Home.jsx
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from "react";
-import "../index.css";
-import logo from "../assets/aossie_logo.png";
-import starsImg from "../assets/stars.png";
-import arrow from "../assets/arrow.png";
-import gitStar from "../assets/gitStar.png";
-import { FaGithub } from "react-icons/fa";
+import React, { useState, useEffect } from 'react';
+import '../index.css';
+import logo from '../assets/aossie_logo.png';
+import starsImg from '../assets/stars.png';
+import arrow from '../assets/arrow.png';
+import gitStar from '../assets/gitStar.png';
+import { FaGithub } from 'react-icons/fa';
 
 const Home = () => {
   const [stars, setStars] = useState(null);
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
 
   async function fetchGitHubStars() {
     const response = await fetch(
-      "https://api.github.com/repos/AOSSIE-Org/EduAid"
+      'https://api.github.com/repos/AOSSIE-Org/EduAid'
     );
     if (!response.ok) {
-      throw new Error("Failed to fetch stars");
+      throw new Error('Failed to fetch stars');
     }
     const data = await response.json();
     return data.stargazers_count;
@@ -27,8 +27,8 @@ const Home = () => {
   }
 
   useEffect(() => {
-    const storedStars = localStorage.getItem("stars");
-    const storedTime = localStorage.getItem("fetchTime");
+    const storedStars = localStorage.getItem('stars');
+    const storedTime = localStorage.getItem('fetchTime');
     if (
       storedStars &&
       storedTime &&
@@ -39,11 +39,11 @@ const Home = () => {
       fetchGitHubStars()
         .then((starCount) => {
           setStars(starCount);
-          localStorage.setItem("stars", starCount);
-          localStorage.setItem("fetchTime", Date.now().toString());
+          localStorage.setItem('stars', starCount);
+          localStorage.setItem('fetchTime', Date.now().toString());
         })
         .catch((err) => {
-          setError("Failed to fetch stars");
+          setError('Failed to fetch stars');
         });
     }
   }, []);
@@ -52,7 +52,11 @@ const Home = () => {
     <div className="popup w-screen h-screen bg-[#02000F] flex justify-center items-center">
       <div className="w-full h-full bg-cust bg-opacity-50 bg-custom-gradient">
         <div>
-          <img src="aossie_transparent.png" alt="logo" className="w-24 my-6 mx-6 block" />
+          <img
+            src="aossie_transparent.png"
+            alt="logo"
+            className="w-24 my-6 mx-6 block"
+          />
           <div className="text-7xl text-center font-extrabold">
             <span className="bg-gradient-to-r from-[#FF005C] to-[#7600F2] text-transparent bg-clip-text">
               Edu
@@ -64,7 +68,7 @@ const Home = () => {
           <div className="text-white text-[1.5rem] text-center my-4">
             <div>A tool that can auto-generate short quizzes</div>
             <div className="flex text-center justify-center gap-4">
-              based on user input{" "}
+              based on user input{' '}
               <img src={starsImg} width={32} height={12} alt="" />
             </div>
           </div>
@@ -94,7 +98,7 @@ const Home = () => {
             <div className="mt-8 rounded-2xl">
               <a href="question-type">
                 <button className="items-center text-lg flex justify-center gap-4 text-white px-6 py-3 mx-auto mt-6 border-gradient hover:wave-effect rounded-md">
-                  Let’s get Started{" "}
+                  Let’s get Started{' '}
                   <img src={arrow} width={28} height={24} alt="" />
                 </button>
               </a>
