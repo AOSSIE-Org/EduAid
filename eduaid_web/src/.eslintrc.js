ea9f9600ddc9bab1f4774c743d3f21690ed77d7c
Ibrahim Habib <ibrahimhabib.eg@gmail.com>
2025-04-07 17:25:25 +0200

    Update ESLint config to use React recommended rules

--- eduaid_web/src/.eslintrc.js
+++ eduaid_web/src/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: "eslint:recommended",
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
