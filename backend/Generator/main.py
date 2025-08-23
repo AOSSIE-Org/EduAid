4b277c33264e07ce1a4c3112825951343399d5cf
Yuvika Gupta <154015713+yuvikaaaaaa@users.noreply.github.com>
2025-08-24 02:35:35 +0530

    Update main.py

--- backend/Generator/main.py
+++ backend/Generator/main.py
@@ -457,9 +457,10 @@ class QuestionGenerator:
         """
 
         VALID_ANSWER_STYLES = ["all", "sentences", "multiple_choice"]
+        
         if answer_style not in VALID_ANSWER_STYLES:
             raise ValueError(
-                "Invalid answer style {}. Please Choose from {}".format(
+                "Invalid answer style {}. Please choose from {}".format(
                     answer_style, VALID_ANSWER_STYLES
                 )
             )
