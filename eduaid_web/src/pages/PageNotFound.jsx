0308bd7f9520fbf4e2596524475ec9aeae448fcf
aniket_agrawal <124797476+Aniket132003@users.noreply.github.com>
2026-02-01 11:47:50 +0530

    fix-eslint

--- eduaid_web/src/pages/PageNotFound.jsx
+++ eduaid_web/src/pages/PageNotFound.jsx
@@ -31,7 +31,7 @@ const NotFound = () => {
                 <p className="text-gray-400">
                     Redirecting to home page in <span className="text-blue-400">{countdown}</span> seconds...
                 </p>
-                <button onClick={() => router('/')}className="mt-6 px-4 py-2 rounded bg-blue-400 text-white" aria-label="Go to home">
+                <button onClick={() => router('/')} className="mt-6 px-4 py-2 rounded bg-blue-400 text-white" aria-label="Go to home">
                 Go Home Now
                 </button>
             </div>
