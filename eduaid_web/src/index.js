abcedc8a73718ac86ec8b1184a6a60a29e8a07c7
Mahek Gupta <guptamahek761@gmail.com>
2026-02-21 21:52:13 +0530

    fix(electron): remove invalid renderer electron detection

--- eduaid_web/src/index.js
+++ eduaid_web/src/index.js
@@ -4,11 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//  Detect Electron
-if (window && window.process && window.process.type) {
-  document.body.classList.add("electron");
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
